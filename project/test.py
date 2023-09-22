1bff306fcbfa97a8fed4d9f2a1037d42ebbe4e7f
ReVonW <92338878+ReVonW@users.noreply.github.com>
2023-09-22 15:20:52 -0500

    Add files via upload

---
+++ project/test.py
@@ -0,0 +1,16 @@
+import unittest 
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+
+    def test_list_int(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
